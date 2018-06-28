fd7f2fb01d01a9e8be92d515d5ed439e9c2fdfe6
Ramenable <40548649+Ramenable@users.noreply.github.com>
2018-06-28 14:08:26 -0400

    Add files via upload

---
+++ Path.java
@@ -0,0 +1,20 @@
+
+public class Path {
+
+	public static void main(String[] args) {
+		
+		String value = System.getenv("PATH");
+		
+		//System.out.println(value);
+		
+		String [] parts = value.split(":");
+		
+		for (String each : parts) {
+			System.out.println(each);
+		}
+		
+		
+
+	}
+
+}
